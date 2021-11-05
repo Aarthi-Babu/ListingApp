8f513a01d71dd685adafdbad5785188f57584cba
Aarthi <aarthibabu1212@gmail.com>
2021-11-05 20:16:29 +0530

    splash screen

--- app/src/main/java/com/example/listingapp/ListingActivity.kt
+++ app/src/main/java/com/example/listingapp/ListingActivity.kt
@@ -1,11 +1,12 @@
 package com.example.listingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ListingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        installSplashScreen()
         setContentView(R.layout.activity_listing)
     }
 }
--- app/src/main/java/com/example/listingapp/SplashScreenActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.listingapp
-
-class SplashScreenActivity {
-}
