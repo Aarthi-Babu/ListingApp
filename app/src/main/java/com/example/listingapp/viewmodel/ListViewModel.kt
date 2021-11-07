691c49e4e3914f3030d1544037bd705d859d9ebb
Aarthi <aarthibabu1212@gmail.com>
2021-11-07 20:40:43 +0530

    second screen UI

--- app/src/main/java/com/example/listingapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/listingapp/viewmodel/ListViewModel.kt
@@ -24,6 +24,7 @@ class ListViewModel : ViewModel() {
                         User(
                             it.cell, it.name.first,
                             it.name.last
+//                            it.picture.thumbnail
                         )
                     }
                     if (user != null) {
